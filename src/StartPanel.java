bd75fd6e004a1ad97ef9bb46b423af251997344a
Pofilo <vincent.merat@utbm.fr>
2014-05-19 15:53:13 +0200

    Ajout des boutons dans la classe StartPanel

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -8,22 +9,37 @@ import javax.swing.JPanel;
 public class StartPanel extends JPanel{
 	public StartPanel(){
 		super();
-		this.setBackground(Color.green);
+		this.setBackground(Color.white);
 		
 		/* Definition of the GridLayout */
 		GridLayout gl = new GridLayout();
 		gl.setColumns(2);
 		gl.setRows(3);
-		gl.setHgap(5);
-		gl.setVgap(5);
+		gl.setHgap(10);
+		gl.setVgap(10);
 		
 		/* Adding of the GridLayout to RulesPanel */
 		this.setLayout(gl);
 		
-		this.add(new JButton("Easy"));
-		this.add(new JButton("Medium"));
-		this.add(new JButton("Difficult"));
-		this.add(new JButton("Custom"));
-		this.add(new JButton("Infinite"));
+		/* Creation of the buttons */
+		JButton btEasy = new JButton();
+		JButton btMedium = new JButton();
+		JButton btDifficult = new JButton();
+		JButton btCustom = new JButton();
+		JButton btInfinite = new JButton();
+		
+		/* Setting the buttons' texts */
+		btEasy.setText("Easy");
+		btMedium.setText("Medium");
+		btDifficult.setText("Difficult");
+		btCustom.setText("Custom");
+		btInfinite.setText("Infinite");
+		
+		/* Adding the buttons to the panel */
+		this.add(btEasy);
+		this.add(btMedium);
+		this.add(btDifficult);
+		this.add(btCustom);
+		this.add(btInfinite);
 	}
 }
