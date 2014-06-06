d1cfcfe3a250c21d077bff34133d06b044e16f06
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2014-06-06 23:00:08 +0200

    2nde étape du passage à la taille de grille dynamique + organisation et commentage de certaines classes

--- src/view/CasePanel.java
+++ src/view/CasePanel.java
@@ -1,17 +1,21 @@
 package view;
 import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
 import model.Case;
-
 import control.WindowManager;
 
 
 public class CasePanel extends JPanel {
+	/*
+	 * Attributes
+	 */
+	
+	//Loading all the images
 	private JPanel hiddenImg = new ImagePanel("images/png/hidden.png");
-	private JPanel emptyImg = new ImagePanel("");
+	private JPanel emptyImg = new ImagePanel("images/png/empty.png");
 	private JPanel flagImg = new ImagePanel("images/png/flag.png");
 	private JPanel askImg = new ImagePanel("images/png/ask.png");
 	private JPanel bombImg = new ImagePanel("images/png/bomb.png");
@@ -25,17 +29,28 @@ public class CasePanel extends JPanel {
 	private JPanel sevenImg = new ImagePanel("images/png/seven.png");
 	private JPanel eightImg = new ImagePanel("images/png/eight.png");
 	
+	/*
+	 * Constructor
+	 */
 	public CasePanel(String xy) {
 		super();
-		this.setSize(32, 32);
+		this.setPreferredSize(new Dimension(32, 32));
 		this.setBackground(new Color(190,189,188));
-		this.addMouseListener(WindowManager.gc);
 		this.setName(xy);
+		this.setLayout(null);
 		this.updateCase(Case.Content.empty, Case.State.hidden);
+		this.addMouseListener(WindowManager.gc);
 	}
 	
+	/*
+	 * Methods
+	 */
+	
 	protected void updateCase(Case.Content content, Case.State state) {
+		/* First, remove the previous image */
 		this.removeAll();
+		
+		/* Then pick the new one and past it */
 		switch(state) {
 			case hidden:
 				this.add(this.hiddenImg);
@@ -81,13 +96,9 @@ public class CasePanel extends JPanel {
 				}
 				break;
 		}
-		System.out.print("repaint (");
-		System.out.print(this.getName());
-		System.out.print(" & ");
-		System.out.print(state);
-		System.out.print(" & ");
-		System.out.print(content);
-		System.out.println(")");
+		
+		/* And finely, we correctly position the image and refresh the panel */
+		this.getComponent(0).setBounds(0, 0, 32, 32);
 		this.updateUI();
 	}
 }
--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -17,30 +17,42 @@ public class GamePanel extends JPanel implements UpdateDisplayListener {
 	public int sizeY;
 	
 	public GamePanel(int sizeX, int sizeY) {
+		/* Initializations */
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
-		this.setSize(new Dimension(this.sizeY*32,this.sizeX*32));
-		panel = new JPanel();
-		panel.setLayout(new GridLayout(this.sizeY,this.sizeX,-5,-5));
+		this.setPreferredSize(new Dimension(this.sizeY*32,this.sizeX*32));
 		this.setBackground(Color.white);
+		
+		/* Initialization of the global Panel */
+		this.panel = new JPanel();
+		this.panel.setLayout(null);
+		this.panel.setPreferredSize(new Dimension(this.sizeY*32,this.sizeX*32));
+		this.add(this.panel);
+		
+		/* Updating global variable */
 		WindowManager.iconSize = this.calculateIconSize();
-		for(int i=0;i<this.sizeY;i++) {
-			for(int j=0;j<this.sizeX;j++)
-				panel.add(new CasePanel(String.valueOf(j).concat("#").concat(String.valueOf(i))));
-		}
-		this.add(panel);
 		WindowManager.udc.add(this);
+		
+		/* Filling the panel with all the cases, and setting their position */
+		for(int i=0;i<this.sizeY;i++) {
+			for(int j=0;j<this.sizeX;j++) {
+				this.panel.add(new CasePanel(String.valueOf(j).concat("#").concat(String.valueOf(i))));
+				this.panel.getComponent(j+this.sizeX*i).setBounds(j*32, i*32, 32, 32);
+			}
+		}
 	}
 	
 	public int calculateIconSize() {
-		return 0;
+		return 32;
 	}
 	
+	/* Before we repaint the component, we recalculate the icon's size*/
 	public void paintComponent(Graphics g) {
 		WindowManager.iconSize = this.calculateIconSize();
 		super.paintComponent(g);
 	}
 	
+	/* Implementation of UpdateDisplayListener */
 	public void updateCase(int x, int y, Case.Content content, Case.State state) {
 		((CasePanel) panel.getComponents()[x+(this.sizeX)*y]).updateCase(content, state);
 	}
