d96ccb55df457ca71b88fe624a8baf8b73f448f7
Antoine Lamielle <antoine.lamielle@0x010c.fr>
2014-06-14 11:03:52 +0200

    'Résolution' du warning dans la mainclass

--- src/control/MainClass.java
+++ src/control/MainClass.java
@@ -3,5 +3,6 @@ package control;
 public class MainClass {
 	public static void main(String[] args) {
 		WindowManager mainManager = new WindowManager();
+		mainManager.toString(); //only useful to remove the "unused attribute" warning
 	}
 }
